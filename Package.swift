3976dcfcd84edb02009fdc736b4716b830c7d938
Jan Krukowski <jan.krukowski@geneva.com>
2024-08-19 19:04:30 +0200

    fixed spi build, added license

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SQLiteVec",
+    platforms: [
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
+    ],
     products: [
         .executable(
             name: "SQLiteVecCLI",
